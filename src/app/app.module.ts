a63f26f56c8753687b6f34a71cc18a4b1baac29a
Gaurav Chalana <gauravchalana.ece@gmail.com>
2020-08-04 18:25:18 -0500

    Designed the structure of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftSideNavComponent } from './left-side-nav/left-side-nav.component';
+import { EducationComponent } from './left-side-nav/education/education.component';
+import { ExperienceComponent } from './left-side-nav/experience/experience.component';
+import { ProjectsComponent } from './left-side-nav/projects/projects.component';
+import { CertificationsComponent } from './left-side-nav/certifications/certifications.component';
+import { SkillsComponent } from './left-side-nav/skills/skills.component';
+import { ContactComponent } from './left-side-nav/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftSideNavComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    CertificationsComponent,
+    SkillsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
